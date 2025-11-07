2eba3a02068162dcbdd2c205d1d58f6bb9194990
nuuuwan <nuuuwan@gmail.com>
2025-11-07 12:05:55 +0530

    Skip slow test for duration writing in TestCase

--- tests/test_journey.py
+++ tests/test_journey.py
@@ -9,7 +9,7 @@ TEST_JOURNEY = Journey.from_route_now(TEST_JOURNEY_ROUTE)
 
 
 class TestCase(unittest.TestCase):
-
+    @unittest.skip("Slow")
     def test_write_duration(self):
         TEST_JOURNEY.write_duration()
         self.assertTrue(os.path.exists(TEST_JOURNEY.data_path))
