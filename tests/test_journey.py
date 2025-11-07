c49bf20358297541db04d416a629d5e4ea101151
nuuuwan <nuuuwan@gmail.com>
2025-11-07 15:58:09 +0530

    Fix test case to call write_journey_info instead of write_duration

--- tests/test_journey.py
+++ tests/test_journey.py
@@ -11,5 +11,5 @@ TEST_JOURNEY = Journey.from_route_now(TEST_JOURNEY_ROUTE)
 class TestCase(unittest.TestCase):
     @unittest.skip("Slow")
     def test_write_duration(self):
-        TEST_JOURNEY.write_duration()
+        TEST_JOURNEY.write_journey_info()
         self.assertTrue(os.path.exists(TEST_JOURNEY.data_path))
