727772834629fab8a712d13178709ebfbc6b9111
nuuuwan <nuuuwan@gmail.com>
2025-11-07 13:01:25 +0530

    Refactor build_readme method to utilize get_lines_for_routes for improved readability

--- src/cmb_traffic/TrafficIndex.py
+++ src/cmb_traffic/TrafficIndex.py
@@ -24,8 +24,19 @@ class TrafficIndex:
             journey = Journey.from_route_now(route)
             journey.write_duration()
 
+    def get_lines_for_routes(self) -> list[str]:
+        lines = ["## Routes", ""]
+        for route in self.undirected_journey_route_list:
+            lines.extend([f"### {route.name}", ""])
+            chart_path = route.build_chart()
+            lines.extend([f"![{chart_path}]({chart_path})", ""])
+        return lines
+
     def build_readme(self):
-        lines = ["Colombo Traffic Index (cmb_traffic)", ""]
+        lines = [
+            "# Colombo Traffic Index (cmb_traffic)",
+            "",
+        ] + self.get_lines_for_routes()
         readme_file = File(self.README_PATH)
         readme_file.write_lines(lines)
         log.info(f"Wrote {readme_file}")
