51fa8fc0a309c668a501135ad7238389462acf58
nuuuwan <nuuuwan@gmail.com>
2025-11-09 15:23:51 +0530

    Add PlotUtils class for enhanced plotting functionality

---
+++ src/utils_future/PlotUtils.py
@@ -0,0 +1,27 @@
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from utils import File, Log
+
+from utils_future.TimeUtils import TimeUtils
+
+log = Log("PlotUtils")
+
+
+class PlotUtils:
+    @staticmethod
+    def write(plot_path: str):
+        plt.figure(figsize=(8, 4.5))
+        ax = plt.gca()
+        ax.xaxis.set_major_formatter(
+            mdates.DateFormatter("%Y-%m-%d %H:%M", tz=TimeUtils.LK_TZ)
+        )
+        ax.xaxis.set_major_locator(MaxNLocator(nbins=7))
+        plt.legend()
+        plt.grid(True, alpha=0.1)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(plot_path, dpi=150)
+        plt.close()
+        log.info(f"Wrote {File(plot_path)}")
+        return plot_path
