f2a4fd527a1a0273c6af522aee530eb114876ae4
nuuuwan <nuuuwan@gmail.com>
2025-11-09 14:47:06 +0530

    Update build_readme_only.sh to use dynamic commit message for local runs

--- workflows/build_readme_only.sh
+++ workflows/build_readme_only.sh
@@ -1,6 +1,8 @@
 # !/bin/zsh
-
+$MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
+echo $MSG
 python3 workflows/build_readme_only.py ; 
 git add images; 
 git add README.md; 
-git commit -m "Ran build_readme_only.py locally"
+git commit -m $MSG
+echo '---"
