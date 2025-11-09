0799e8960e1a6f1d29db1179c09998b4ea7934a3
nuuuwan <nuuuwan@gmail.com>
2025-11-09 14:50:35 +0530

    Fix variable assignment in build_readme_only.sh to ensure MSG is properly set

--- workflows/build_readme_only.sh
+++ workflows/build_readme_only.sh
@@ -1,5 +1,5 @@
 # !/bin/zsh
-$MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
+MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
 echo $MSG
 python3 workflows/build_readme_only.py ; 
 git add images; 
