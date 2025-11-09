5e948d28fd567cfdc8e6d3e5099b6253c7241055
nuuuwan <nuuuwan@gmail.com>
2025-11-09 14:53:10 +0530

    Remove build_readme_only.sh script and update .gitignore to include shell scripts

--- workflows/build_readme_only.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/zsh
-MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
-echo $MSG
-python3 workflows/build_readme_only.py ; 
-git add images; 
-git add README.md; 
-git commit -m $MSG
-echo '---"
