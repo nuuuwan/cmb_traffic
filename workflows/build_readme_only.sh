d9a9ecc3010c402329741ba293fa6ed2e498cba6
nuuuwan <nuuuwan@gmail.com>
2025-11-09 14:55:18 +0530

    Fix shebang to use python3 in build_readme_only.sh

--- workflows/build_readme_only.sh
+++ workflows/build_readme_only.sh
@@ -1,6 +1,6 @@
 # !/bin/zsh
 MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
-python workflows/build_readme_only.py ; 
+python3 workflows/build_readme_only.py ; 
 git add images; 
 git add README.md; 
 git commit -m $MSG
