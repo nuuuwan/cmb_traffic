ac7f43d4a29756e0acae6a23aaf4d6d292c5de5b
nuuuwan <nuuuwan@gmail.com>
2025-11-11 11:03:57 +0530

    Add TIME_FORMAT constant to TrafficIndexReadMeMixin for consistency

--- src/cmb_traffic/traffic_index/TrafficIndexReadMeMixin.py
+++ src/cmb_traffic/traffic_index/TrafficIndexReadMeMixin.py
@@ -15,6 +15,7 @@ class TrafficIndexReadMeMixin(
 
     DIR_IMAGES = JourneyRoute.DIR_IMAGES
     README_PATH = "README.md"
+    TIME_FORMAT = "%Y-%m-%d %H:%M"
 
     @staticmethod
     def get_time_updated_for_badge(journey_d_list) -> str:
