2e2f3bb1a6fdc4046b99a096952457818d2f828a
nuuuwan <nuuuwan@gmail.com>
2025-11-11 11:16:39 +0530

    Update date formatting in PlotUtils to use short format and adjust x-axis locator for improved clarity

--- src/utils_future/PlotUtils.py
+++ src/utils_future/PlotUtils.py
@@ -10,18 +10,23 @@ log = Log("PlotUtils")
 
 class PlotUtils:
     TIME_FORMAT = "%Y-%m-%d (%a) %H:%M"
+    TIME_FORMAT_SHORT = "%Y-%m-%d"
 
     @staticmethod
     def write(plot_path: str):
         ax = plt.gca()
         ax.xaxis.set_major_formatter(
-            mdates.DateFormatter(PlotUtils.TIME_FORMAT, tz=TimeUtils.LK_TZ)
+            mdates.DateFormatter(
+                PlotUtils.TIME_FORMAT_SHORT, tz=TimeUtils.LK_TZ
+            )
         )
-        ax.xaxis.set_major_locator(MaxNLocator(nbins=7))
+        ax.xaxis.set_major_locator(MaxNLocator(nbins=5))
+        plt.tight_layout()
         plt.legend()
         plt.grid(True, alpha=0.1)
-        plt.xticks(rotation=45)
+        plt.xticks()
         plt.savefig(plot_path, dpi=150)
         plt.close()
+
         log.info(f"Wrote {File(plot_path)}")
         return plot_path
