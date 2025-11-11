4d17524b28f93dd18ff2e00aeeb970944779cfac
nuuuwan <nuuuwan@gmail.com>
2025-11-11 12:12:24 +0530

    Refactor build_readme_only.sh to streamline git commands and improve script clarity

--- workflows/build_readme_only.sh
+++ workflows/build_readme_only.sh
@@ -1,7 +1,12 @@
 # !/bin/zsh
-MSG="🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')"
-python3 workflows/build_readme_only.py ; 
+echo_cmd git reset --hard HEAD;
+echo_cmd git clean -fd;
+git status;
+git pull --rebase origin main;
+python3 workflows/build_readme_only.py; 
 git add images; 
 git add README.md; 
-git commit -m "$MSG"
+git commit -m "🤖-local [build_readme_only.py] $(date '+%Y-%m-%d-%H%M')";
+git pull --rebase origin main;
+git push origin main;
 
