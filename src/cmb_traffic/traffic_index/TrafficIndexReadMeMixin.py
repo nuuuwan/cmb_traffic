7c3a964824d3613efcf814073872a34ebfb382fd
nuuuwan <nuuuwan@gmail.com>
2025-11-11 11:05:45 +0530

    Update TIME_FORMAT in TrafficIndexReadMeMixin to include day of the week

--- src/cmb_traffic/traffic_index/TrafficIndexReadMeMixin.py
+++ src/cmb_traffic/traffic_index/TrafficIndexReadMeMixin.py
@@ -15,7 +15,7 @@ class TrafficIndexReadMeMixin(
 
     DIR_IMAGES = JourneyRoute.DIR_IMAGES
     README_PATH = "README.md"
-    TIME_FORMAT = "%Y-%m-%d %H:%M"
+    TIME_FORMAT = "%Y-%m-%d (%a) %H:%M"
 
     @staticmethod
     def get_time_updated_for_badge(journey_d_list) -> str:
