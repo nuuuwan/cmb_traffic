d455c6835ac89d5ae64442669ab2e3cd6585d14c
nuuuwan <nuuuwan@gmail.com>
2025-11-11 10:26:35 +0530

    Add Colombo district information to location details for clarity

--- src/cmb_traffic/Location.py
+++ src/cmb_traffic/Location.py
@@ -56,17 +56,17 @@ class Location:
 Location.BAMBALAPITIYA = Location(
     name="Bambalapitiya",
     latlng=LatLng(6.895572468746244, 79.85483770889027),
-    details="Bambalapitiya Junction on Galle Road",
+    details="Bambalapitiya Junction on Galle Road (Colombo 4)",
 )
 Location.BORELLA = Location(
     name="Borella",
     latlng=LatLng(6.910882574522934, 79.88789773709671),
-    details="Ayurveda Junction on Sri Jayewardenepura Mawatha",
+    details="Ayurveda Junction on Sri Jayewardenepura Mawatha (Colombo 8)",
 )
 Location.DEMATAGODA = Location(
     name="Dematagoda",
     latlng=LatLng(6.943175860321491, 79.87820817923517),
-    details="Southside of Dematagoda Canal Bridge, on A1/Baseline Road",
+    details="Southside of Dematagoda Canal Bridge, on A1/Baseline Road (Colombo 9)",
 )
 Location.FORT = Location(
     name="Fort",
@@ -76,15 +76,15 @@ Location.FORT = Location(
 Location.MATTAKKULIYA = Location(
     name="Mattakkuliya",
     latlng=LatLng(6.980026983331188, 79.87551282104877),
-    details="Southside of Mattakkuliya Bridge, on New Negombo Road",
+    details="Southside of Mattakkuliya Bridge, on New Negombo Road (Colombo 15)",
 )
 Location.PAMANKADA = Location(
     name="Pamankada",
     latlng=LatLng(6.871812810816128, 79.88456400975986),
-    details="High Level Road border of CMC",
+    details="High Level Road border of CMC (Colombo 6)",
 )
 Location.WELLAWATTE = Location(
     name="Wellawatte",
     latlng=LatLng(6.863288956321618, 79.86360827087549),
-    details="Northside of Dehiwala Bridge, on Galle Road",
+    details="Northside of Dehiwala Bridge, on Galle Road (Colombo 6)",
 )
