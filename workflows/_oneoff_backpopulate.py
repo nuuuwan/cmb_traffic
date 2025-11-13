9659274060d1959e75b869d87bfd8f26ec28682e
nuuuwan <nuuuwan@gmail.com>
2025-11-13 18:07:01 +0530

    Refactor backpopulate script to improve logic flow and ensure updates are only written when changes occur.

--- workflows/_oneoff_backpopulate.py
+++ workflows/_oneoff_backpopulate.py
@@ -3,7 +3,7 @@ from dataclasses import asdict
 
 from utils import JSONFile, Log, Time, TimeFormat
 
-from cmb_traffic import Location, Route
+from cmb_traffic import Journey, Location, Route
 
 log = Log("_oneoff_backpopulate")
 
@@ -16,10 +16,11 @@ def main():
                 continue
             file_path = os.path.join(dir_parent, file_name)
             json_file = JSONFile(file_path)
-            log.debug(f"Processing {json_file}")
             d = json_file.read()
 
+            has_updated = False
             if "name" in d:
+                has_updated = True
                 route_name = d["name"]
                 start_location_name, end_location_name = route_name.split(
                     " to "
@@ -43,10 +44,16 @@ def main():
                     del d["end_location"]
 
             if "start_time" in d:
+                has_updated = True
                 d["ut_start"] = d["start_time"]
                 del d["start_time"]
 
-            json_file.write(d)
+            if has_updated:
+                json_file.write(d)
+                log.warning(f"Updated {json_file}")
+
+            journey = Journey.from_file_path(file_path)
+            journey.write()
 
 
 if __name__ == "__main__":
