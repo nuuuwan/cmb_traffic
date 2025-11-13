6a25207c848bc06d6356f031bf28b6dadb4e0f13
nuuuwan <nuuuwan@gmail.com>
2025-11-13 17:56:35 +0530

    Implement backpopulate script for journey data processing and remove obsolete direct speed script

---
+++ workflows/_oneoff_backpopulate.py
@@ -0,0 +1,47 @@
+import os
+from dataclasses import asdict
+
+from utils import JSONFile, Log, Time, TimeFormat
+
+from cmb_traffic import Location, Route
+
+log = Log("_oneoff_backpopulate")
+
+
+def main():
+    dir_root = os.path.join("data", "journeys")
+    for dir_parent, __, file_names in os.walk(dir_root):
+        for file_name in file_names:
+            if not file_name.endswith(".json"):
+                continue
+            file_path = os.path.join(dir_parent, file_name)
+            json_file = JSONFile(file_path)
+            log.debug(f"Processing {json_file}")
+            d = json_file.read()
+
+            if "name" in d:
+                route_name = d["name"]
+                start_location_name, end_location_name = route_name.split(
+                    " to "
+                )
+                start_location = Location.from_name(start_location_name)
+                end_location = Location.from_name(end_location_name)
+                route = Route(
+                    start_location=start_location,
+                    end_location=end_location,
+                )
+                d["route"] = asdict(route)
+
+                del d["name"]
+                del d["start_latlng"]
+                del d["end_latlng"]
+
+            if "start_time" in d:
+                d["ut_start"] = d["start_time"]
+                del d["start_time"]
+
+            json_file.write(d)
+
+
+if __name__ == "__main__":
+    main()
